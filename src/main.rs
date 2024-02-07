e74f285db59a613285bc0c73781c9c38628ca01b
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2024-02-07 23:00:25 +0530

    Flash screen implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use crossterm::event::{Event, KeyCode, KeyEvent};
-use crossterm::{event, terminal};
+use crossterm::terminal::ClearType;
+use crossterm::{cursor, event::*};
+use crossterm::{event, execute, terminal};
+use std::io::stdout;
 use std::time::Duration;
 
 struct Cleanup;
@@ -7,30 +9,90 @@ struct Cleanup;
 impl Drop for Cleanup {
     fn drop(&mut self) {
         terminal::disable_raw_mode().expect("Could not turn off raw mode");
+        Output::clear_screen().expect("Error");
+    }
+}
+
+struct Reader;
+
+impl Reader {
+    fn read_key(&self) -> crossterm::Result<KeyEvent> {
+        loop {
+            if event::poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = event::read()? {
+                    println!("{:?}\r", event);
+                    return Ok(event);
+                }
+            }
+        }
+    }
+}
+
+struct Output {
+    win_size: (usize, usize),
+}
+
+impl Output {
+    fn new() -> Self {
+        let win_size = terminal::size()
+            .map(|(x, y)| (x as usize, y as usize))
+            .unwrap();
+        Self { win_size }
+    }
+
+    fn clear_screen() -> crossterm::Result<()> {
+        execute!(stdout(), terminal::Clear(ClearType::All));
+        execute!(stdout(), cursor::MoveTo(0, 0))
+    }
+
+    fn draw_rows(&self) {
+        let screen_rows = self.win_size.1;
+        for _ in 0..screen_rows {
+            println!("~\r");
+        }
+    }
+
+    fn refresh_screen(&self) -> crossterm::Result<()> {
+        Self::clear_screen()?;
+        self.draw_rows();
+        execute!(stdout(), cursor::MoveTo(0, 0))
+    }
+}
+
+struct Editor {
+    reader: Reader,
+    output: Output,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self {
+            reader: Reader,
+            output: Output::new(),
+        }
+    }
+
+    fn process_keypress(&self) -> crossterm::Result<bool> {
+        match self.reader.read_key()? {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: event::KeyModifiers::CONTROL,
+            } => return Ok(false),
+            _ => {}
+        }
+        Ok(true)
+    }
+
+    fn run(&self) -> crossterm::Result<bool> {
+        self.output.refresh_screen()?;
+        self.process_keypress()
     }
 }
 
 fn main() -> crossterm::Result<()> {
     let _cleanup = Cleanup;
     terminal::enable_raw_mode()?;
-    loop {
-        if event::poll(Duration::from_millis(500))? {
-            if let Event::Key(event) = event::read()? {
-                match event {
-                    KeyEvent {
-                        code: KeyCode::Char('q'),
-                        modifiers: event::KeyModifiers::NONE,
-                    } => break,
-                    _ => {
-                        //todo
-                    }
-                }
-                println!("{:?}\r", event);
-            };
-        } else {
-            println!("No input yet\r");
-        }
-        /*end*/
-    }
+    let editor = Editor::new();
+    while editor.run()? {}
     Ok(())
 }
